f87359a6604f1f654eec88640fd255b455c306b0
Upendra Sahu <upendrasahu1199@gmail.com>
2024-10-09 23:08:30 +0530

    Updates

--- src/pages/SystemDesign.jsx
+++
@@ -1,9 +0,0 @@
-import DataTable from "../components/DataTable"
-import { dataArray } from "../data/data"
-const SystemDesign = () => {
-  return (
-    <DataTable dataArray={dataArray} page='SystemDesign'/>
-  )
-}
-
-export default SystemDesign
